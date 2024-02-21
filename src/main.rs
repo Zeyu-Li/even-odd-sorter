bbf2bdad662406d458d927eaed5aa3452c4cd790
Andrew <zeyu.li23@gmail.com>
2024-02-21 12:36:34 -0600

    feat: fixed bug of not updating immediate value

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn even_odd_sort(arr: &mut [i32]) {
         let mut odd_sorted = vec![];
         for handle in handles {
             let (arr_result, sorted_local, is_even) = handle.join().unwrap();
-            if (is_even) {
+            if is_even {
                 even_sorted = arr_result;
             } else {
                 odd_sorted = arr_result;
@@ -49,13 +49,29 @@ fn even_odd_sort(arr: &mut [i32]) {
             sorted = sorted && sorted_local;
         }
 
-        println!("{:?}", even_sorted);
-        println!("{:?} {:?}", odd_sorted, sorted);
+        // println!("{:?}", even_sorted);
+        // println!("{:?} {:?}", odd_sorted, sorted);
 
+        // merge the two
         for i in 0..even_sorted.len()/2 + 1 {
             arr[i * 2] = even_sorted[i * 2];
-            if (i*2+1 < even_sorted.len() - 1) {
+            if i*2+1 < even_sorted.len() - 1 {
                 arr[i * 2 + 1] = odd_sorted[i * 2 + 1];
+            }
+        }
+
+        // offset
+        for i in (1..arr.len() - 1).step_by(2) {
+            if arr[i] > arr[i + 1] {
+                arr.swap(i, i + 1);
+                sorted = false;
+            }
+        }
+        // println!("{:?}", arr);
+        for i in (0..arr.len() - 1).step_by(2) {
+            if arr[i] > arr[i + 1] {
+                arr.swap(i, i + 1);
+                sorted = false;
             }
         }
     }
